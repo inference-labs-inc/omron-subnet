5d9c2158a67d825f6231425373954d5f1c88bb38
HudsonGraeme <spencer@inferencelabs.com>
2024-12-04 18:00:34 +0000

    Update EZKL proof verification

--- neurons/execution_layer/proof_handlers/ezkl_handler.py
+++ neurons/execution_layer/proof_handlers/ezkl_handler.py
@@ -22,7 +22,7 @@ class EZKLHandler(ProofSystemHandler):
 
     def gen_input_file(self, session: VerifiedModelSession):
         bt.logging.trace("Generating input file")
-        input_data = session.inputs.to_array()
+        input_data = session.inputs
         data = {"input_data": input_data}
         os.makedirs(os.path.dirname(session.session_storage.input_path), exist_ok=True)
         with open(session.session_storage.input_path, "w", encoding="utf-8") as f:
