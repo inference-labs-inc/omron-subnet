9cc43bc0bf7aa548f75ac1474fc14acfd1c946eb
HudsonGraeme <spencer@inferencelabs.com>
2024-12-10 19:01:53 +0000

    Add str method for input logging

--- neurons/execution_layer/base_input.py
+++ neurons/execution_layer/base_input.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
 from _validator.models.request_type import RequestType
+import json
 
 
 class BaseInput(ABC):
@@ -46,3 +47,6 @@ class BaseInput(ABC):
     def to_json(self) -> dict[str, object]:
         """Returns the data in JSON-compatible format"""
         return self.data
+
+    def __str__(self) -> str:
+        return json.dumps(self.data)
