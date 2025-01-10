fe58854470adb9f94a177c0cd2547e57b3010243
HudsonGraeme <spencer@inferencelabs.com>
2025-01-10 18:02:48 +0000

    Add await

--- neurons/_validator/validator_session.py
+++ neurons/_validator/validator_session.py
@@ -5,6 +5,7 @@ from _validator.config import ValidatorConfig
 from _validator.core.validator_loop import ValidatorLoop
 from utils import clean_temp_files
 import sys
+import asyncio
 
 
 class ValidatorSession:
@@ -19,7 +20,7 @@ class ValidatorSession:
         bt.logging.debug("Validator session started")
 
         try:
-            self.validator_loop.run()
+            asyncio.run(self.validator_loop.run())
         except KeyboardInterrupt:
             bt.logging.info("KeyboardInterrupt caught. Exiting validator.")
             clean_temp_files()
