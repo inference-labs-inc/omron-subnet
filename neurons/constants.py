7a530b91e48d449c3df63df0cb841749fb2ebb40
HudsonGraeme <spencer@inferencelabs.com>
2025-02-12 21:54:20 +0000

    Bump version to 7.0.0

--- neurons/constants.py
+++ neurons/constants.py
@@ -48,7 +48,7 @@ MAXIMUM_SCORE_MEDIAN_SAMPLE = 0.05
 # Shift in seconds to apply to the minimum response time for vertical asymptote adjustment
 MINIMUM_SCORE_SHIFT = 0.0
 # Weights version hyperparameter
-WEIGHTS_VERSION = 1660
+WEIGHTS_VERSION = 1700
 # Rate limit for weight updates
 WEIGHT_RATE_LIMIT: int = 100
 # Delay between loop iterations
