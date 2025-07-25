2f751acfe458ef3c5d52c7339f08cfe8052eb16d
HudsonGraeme <spencer@inferencelabs.com>
2025-07-25 22:07:08 +0000

    No context manager

--- neurons/_validator/core/capacity_manager.py
+++ neurons/_validator/core/capacity_manager.py
@@ -6,8 +6,8 @@ from protocol import QueryForCapacities
 class CapacityManager:
     def __init__(self, config: ValidatorConfig):
         self.config = config
+        self.dendrite = bt.dendrite(self.config.wallet)
 
     def sync_capacities(self, axons: list[bt.Axon]):
         bt.logging.info(f"Syncing capacities for {len(axons)} axons")
-        with bt.dendrite(self.config.wallet) as dendrite:
-            return dendrite.query(axons, QueryForCapacities())
+        return self.dendrite.query(axons, QueryForCapacities())
